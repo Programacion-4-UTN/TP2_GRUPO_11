44e0b6e953cf9dd192ca3eefa4e05a18ce0c4349
MorenoMat <mateomorenotwelvelive@gmail.com>
2026-09-07 17:15:08 -0300

    Clase Polideportivo

---
+++ src/Ejercicio2/Polideportivo.java
@@ -0,0 +1,46 @@
+package Ejercicio2;
+
+public class Polideportivo implements Edificio, InstalacionDeportiva{
+private String nombre;
+private double superficie;
+ 
+ 
+	public Polideportivo()
+	
+	{
+		this.nombre = "nombreDefault";
+		this.superficie = 0.0;
+	}
+	
+	
+	public Polideportivo(double superficie, String nombre)
+	{
+		this.nombre =nombre;
+		this.superficie = superficie;
+	}
+	@Override
+	public int getTipoDeInstalacion()
+	{
+		return 1;
+	}
+	@Override
+	public double getSuperficieEdificio()
+	{
+		return this.superficie;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setSuperficieEdificio(double superficie) {
+		this.superficie = superficie;
+	}
+	
+}
